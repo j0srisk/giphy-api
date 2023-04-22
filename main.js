a8ec9dda5c052142a59d9d900afa106597b7a150
j0srisk <j0srisk@gmail.com>
2023-04-21 19:55:23 -0500

    Refactored to async functions

--- main.js
+++ main.js
@@ -1,42 +1,28 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-var query = 'Dog'
 
 document.querySelector('#app').innerHTML = `
   <img src="#" alt="GIF">
-  <input type="text" id="search-input" value=` + query + `>
+  <input type="text" id="search-input" value="Swag Monkey">
 `
 
-function loadGif(query) {
-  const img = document.querySelector('img');
-  fetch('https://api.giphy.com/v1/gifs/translate?api_key=QWPzoxlZ0gYPSUgrJzyfeywlk6aeqJrI&s='+query, {mode: 'cors'})
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(response) {
-      img.src = response.data.images.original.url;
-    })
-    .catch(function(error) {
-      alert(error);
-    });
+const searchInput = document.querySelector('input[type="text"]');
+const img = document.querySelector('img');
+
+async function loadGif(query) {
+  const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=QWPzoxlZ0gYPSUgrJzyfeywlk6aeqJrI&s='+query, {mode: 'cors'})
+  const data = await response.json();
+  img.src = data.data.images.original.url;
 }
 
-const searchInput = document.querySelector('input[type="text"]');
-
 searchInput.addEventListener('focusout', function() {
-  query = searchInput.value;
-  loadGif(query);
+  loadGif(searchInput.value);
 });
 
 searchInput.addEventListener('keypress', function(e) {
   if (e.key === 'Enter') {
-    query = searchInput.value;
+    loadGif(searchInput.value);
     searchInput.blur();
-    loadGif(query);
   }
 });
 
-loadGif(query);
+loadGif(document.querySelector('input[type="text"]').value);
